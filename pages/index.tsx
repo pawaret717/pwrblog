b34407f18f9639fc97639b574b16c9420eee73a4
Pawaret Komsakron <pawaret717@gmail.com>
2022-10-23 21:01:51 +0700

    pwr commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,11 +42,15 @@ const Home = ({ posts }: Props) => {
         />
       </div>
 
-      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 mg:gap-6 md:p-6 lg:grid-cols-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post: Post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
             <div className="rounded-lg group cursor-pointer overflow-hidden border">
-              <img src={urlFor(post.mainImage).url()!} alt="" />
+              <img
+                className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
               <div className="flex justify-between bg-white p-5">
                 <div>
                   <p className="text-lg font-bold">{post.title}</p>
