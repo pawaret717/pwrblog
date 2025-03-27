d6cd177d36c198ffd39e6daf5dbc78fe09aa97b7
Pawaret Komsakron <107381822+pawaret717@users.noreply.github.com>
2025-03-28 04:22:40 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home = ({ posts }: Props) => {
         />
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6 max-w-7xl">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post: Post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
             <div className="rounded-lg group cursor-pointer overflow-hidden border">
