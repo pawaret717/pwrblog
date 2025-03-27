fcb2c7fe8bfd2fc57981c9ec7f1f91d6245f1074
Pawaret Komsakron <107381822+pawaret717@users.noreply.github.com>
2025-03-28 04:16:42 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home = ({ posts }: Props) => {
   console.log(posts);
 
   return (
-    <div className="mx-auto max-w-7xl">
+    <div className="mx-auto">
       <Head>
         <title>MUSLIM UP BLOG</title>
         <link rel="icon" href="/favicon.ico" />
