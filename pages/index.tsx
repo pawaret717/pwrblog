32ab250a85b678ab754cf78b4b59cc5aef9ecadf
Pawaret Komsakron <107381822+pawaret717@users.noreply.github.com>
2025-03-28 05:49:15 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,6 +41,10 @@ const Home = ({ posts }: Props) => {
         />
       </div>
 
+      <h4>
+            ล่าสุด
+          </h4>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post: Post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
